d24923f00a8d9dc604bff80aa9144060fdc85145
Sujaany <ssujaany09@gmail.com>
2020-01-27 15:16:05 +0530

    Leave system added

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import './App.css';
+import {Route, Switch } from 'react-router-dom';
+import SideBar from './layout/side-bar';
+import Header from './layout/header';
+import Footer from './layout/footer';
+import LeaveDashboard from './views/leave-system/leave-dashboard';
+
+import LeaveHistory from './views/leave-system/leave-history';
+import LieuLeave from '../src/views/leave-system/lieu-leave-request/leave-lieu';
+import ManageLeaveDashBoard from './views/leave-system/manage-leave-dashboard';
+import ManageLeaveLieu from './views/leave-system/manage-lieu-leave';
+import ManageCarryforward from './views/leave-system/manage-carryforward';
+
+import ConfigureCalendar from './views/settings/configureCalendar';
+import Role from './views/user/role';
+
+
+import ManageIndivitualLeaveHistory from './views/leave-system/manage-indivitual-leave-history';
+import CarryForwardLeaveHistory from './views/leave-system/components/carry-forward-leave-history';
+
+import {Provider} from 'react-redux';
+import store from './store';
+import Users from './views/user/users';
+import ManageLeaveAllocation from './views/leave-system/manage-leave-allocation';
+import ManageProcessLeave from './views/leave-system/manage-process-leave';
+import ManageLeaveTypesAndPolicies from './views/leave-system/manage-leave-type-policy';
+import RecuitmentType from './views/employee/recuitment-type';
+import ManageDesignation from './views/leave-system/manage-Designation';
+import RequestLeave from './views/leave-system/leave-request/index';
+import Login from'./layout/login';
+
+
+
+function App() {
+  return (
+    <Provider store={store}>
+    <div>
+      <Header />
+      <div className="d-flex align-items-stretch">
+        <SideBar />
+        <div className="page-holder w-100 d-flex flex-wrap">
+          <div className="container-fluid px-xl-5">
+
+            <Switch>
+              {/* <Route exact path="/"component={Login}/> */}
+              <Route exact path="/" component={LeaveDashboard} />
+              <Route exact path="/requestLeave" component={RequestLeave} />
+              <Route exact path="/leaveHistory" component={LeaveHistory} />
+              <Route exact path="/lieuLeave" component={LieuLeave} />
+              <Route exact path="/manageLeaveDashboard" component={ManageLeaveDashBoard} />
+              <Route exact path="/manageLeaveRequests" component={ManageProcessLeave} />
+              <Route exact path="/manageLeaveLeiuLeave" component={ManageLeaveLieu} />
+              <Route exact path="/manageCarryForward" component={ManageCarryforward} />
+              <Route exact path="/configurecalendar" component={ConfigureCalendar} />
+              <Route exact path="/manageRole" component={Role}/>
+              <Route exact path="/manageUser" component={Users}/>
+              <Route exact path="/configleave" component={ManageLeaveTypesAndPolicies}/>
+              <Route exact path="/indivitualLeaveHistory" component={ManageIndivitualLeaveHistory}/>
+              <Route exact path="/leaveAllocation" component={ManageLeaveAllocation} />
+              <Route exact path="/recuitmentType" component={RecuitmentType}/>
+              <Route exact path="/designation" component={ManageDesignation}/>
+
+              
+            </Switch>
+
+          </div>
+          <Footer />
+        </div>
+      </div>
+      {/* <Login/> */}
+      </div>
+      </Provider>
+  );
+}
+
+export default App;
---
+++ src/layout/side-bar.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+
+class SideBar extends Component {
+  render() {
+    return (
+
+      <div id="sidebar" className="sidebar py-3">
+        <ul className="sidebar-menu list-unstyled">
+          <li className="sidebar-list-item"><a href="index.html" className="sidebar-link text-muted"><i className="fas fa-home mr-3 text-gray"></i><span>Dashboard</span></a></li>
+          <li className="sidebar-list-item"><a href="#" className="sidebar-link text-muted"><i className="fas fa-home mr-3 text-gray"></i><span>Profile</span></a></li>
+
+          <li className="sidebar-list-item"><a href="#" data-toggle="collapse" data-target="#page1" aria-expanded="false" aria-controls="page1" className="sidebar-link text-muted"><i className="fas fa-th-large mr-3 text-gray"></i><span>Leave</span></a>
+            <div id="page1" className="collapse">
+              <ul className="sidebar-menu list-unstyled border-left border-primary border-thick">
+                <li className="sidebar-list-item"><Link to="/" className="sidebar-link text-muted pl-lg-5">Leave Dashboard</Link></li>
+                <li className="sidebar-list-item"><Link to="/requestLeave" className="sidebar-link text-muted pl-lg-5">Request Leave</Link></li>
+                <li className="sidebar-list-item"><Link to="/lieuLeave" className="sidebar-link text-muted pl-lg-5">Lieu Leave</Link></li>
+                <li className="sidebar-list-item"><Link to="/leaveHistory" className="sidebar-link text-muted pl-lg-5">Leave History</Link></li>
+                <li className="sidebar-list-item"><Link to="/carryforward" className="sidebar-link text-muted pl-lg-5">Carry Forward</Link></li>
+
+
+              </ul>
+            </div>
+          </li>
+          
+          <li className="sidebar-list-item"><a href="#" data-toggle="collapse" data-target="#page2" aria-expanded="false" aria-controls="page2" className="sidebar-link text-muted"><i className="fas fa-list mr-3 text-gray"></i><span>Manage Employee</span></a>
+          <div id="page2" className="collapse">
+              <ul className="sidebar-menu list-unstyled border-left border-primary border-thick">
+                <li className="sidebar-list-item"><Link to="/recuitmentType" className="sidebar-link text-muted pl-lg-5">Recuitment Type</Link></li>
+                <li className="sidebar-list-item"><Link to="/designation" className="sidebar-link text-muted pl-lg-5">Designation</Link></li>
+              </ul>
+              </div>
+          </li>
+          
+          <li className="sidebar-list-item"><a href="#" data-toggle="collapse" data-target="#page3" aria-expanded="false" aria-controls="page3" className="sidebar-link text-muted"><i className="fas fa-list mr-3 text-gray"></i><span>Manage Leave</span></a>
+            <div id="page3" className="collapse">
+              <ul className="sidebar-menu list-unstyled border-left border-primary border-thick">
+                <li className="sidebar-list-item"><Link to="/manageLeaveDashboard" className="sidebar-link text-muted pl-lg-5">Leave Dashboard</Link></li>
+                <li className="sidebar-list-item"><Link to="/configleave" className="sidebar-link text-muted pl-lg-5">Leave Types & Policies</Link></li>
+                <li className="sidebar-list-item"><Link to="/leaveAllocation" className="sidebar-link text-muted pl-lg-5">Leave Allocation</Link></li>
+                <li className="sidebar-list-item"><Link to="/manageLeaveRequests" className="sidebar-link text-muted pl-lg-5">Process Leave</Link></li>
+                <li className="sidebar-list-item"><Link to="/manageLeaveLeiuLeave" className="sidebar-link text-muted pl-lg-5">Lieu Leave</Link></li>
+                <li className="sidebar-list-item"><Link to="/manageCarryForward" className="sidebar-link text-muted pl-lg-5">Carry Forward</Link></li>
+                
+                
+              </ul>
+            </div>
+          </li>
+          <li className="sidebar-list-item"><a href="#" data-toggle="collapse" data-target="#page4" aria-expanded="false" aria-controls="page4" className="sidebar-link text-muted"><i className="fas fa-users-cog mr-3 text-gray"></i><span>Manage User</span></a>
+            <div id="page4" className="collapse">
+              <ul className="sidebar-menu list-unstyled border-left border-primary border-thick">
+                <li className="sidebar-list-item"><Link to="/manageRole" className="sidebar-link text-muted pl-lg-5">Role</Link></li>
+                <li className="sidebar-list-item"><Link to="/manageUser" className="sidebar-link text-muted pl-lg-5">User List</Link></li>
+              </ul>
+            </div>
+          </li>
+
+          <li className="sidebar-list-item"><a href="#" data-toggle="collapse" data-target="#page5" aria-expanded="false" aria-controls="page5" className="sidebar-link text-muted"><i className="fas fa-cogs  mr-3 text-gray"></i><span>Settings</span></a>
+            <div id="page5" className="collapse">
+              <ul className="sidebar-menu list-unstyled border-left border-primary border-thick">
+                <li className="sidebar-list-item"><Link to="/configurecalendar" className="sidebar-link text-muted pl-lg-5">Configure Calendar</Link></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+
+      </div>
+
+    )
+  }
+}
+export default SideBar;
